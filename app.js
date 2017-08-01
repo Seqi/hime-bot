4246402094bdf8e1fb57316a536b74c31decb9e6
seqi <tomseqi@live.co.uk>
2017-08-02 00:17:59 +0100

    Basic image dumper functionality working with shitty messy code. Ignoring vs code files.

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-const config = require(`./config.${process.env.NODE_ENV || 'development'}.js`); 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const config = require(`./config.${process.env.NODE_ENV || 'development'}.js`);
+const gelbooru = require('./gelbooru');
+
+const TAG = 'hime_cut';
+const SFW_TAG = TAG + '+rating:safe';
+const NSFW_TAG = TAG + '+rating:explicit';
+
+const SERVER_IDS = [
+    '338741523380436993'
+];
+
+const SFW_CHANNEL_IDS = [
+    '338741996367773696',
+]
+
+const NSFW_CHANNEL_IDS = [
+    '338742017586757632'
+]
+
+const DEV_SERVER_ID = '341347176930213889';
+
+const IMAGE_POST_INTERVAL_MILLIS = 1000 * 60 * 30;
+
 client.on('ready', () => {
     console.log('Bot connected as %s - %s', client.user.username, client.user.id);
+
+    // Set up SFW
+    const sfwChannels = client.channels.filter(channel => SFW_CHANNEL_IDS.indexOf(channel.id) > -1).array();
+    const nsfwChannels = client.channels.filter(channel => NSFW_CHANNEL_IDS.indexOf(channel.id) > -1).array();
+
+    setInterval(function() {
+        for (let i = 0; i < sfwChannels.length; i++) {
+            gelbooru.getRandomImage(SFW_TAG, (err, result) => {
+                console.log('posting ' + result.file_url + ' to ' + sfwChannels[i].name);
+                sfwChannels[i].send(result.file_url);
+            });
+        }
+    }, IMAGE_POST_INTERVAL_MILLIS);
+
+    setInterval(() => {
+        for (let i = 0; i < nsfwChannels.length; i++) {
+            gelbooru.getRandomImage(NSFW_TAG, (err, result) => {
+                console.log('posting ' + result.file_url + ' to ' + sfwChannels[i].name);
+                nsfwChannels[i].send(result.file_url);
+            });
+        }
+    }, IMAGE_POST_INTERVAL_MILLIS);
 });
 
 client.on('message', msg => {
-    if (msg.author.id !== client.user.id) {
-        msg.channel.sendMessage('Hello world!');
+    if (msg.guild.id == DEV_SERVER_ID && msg.author.id !== client.user.id && msg.content === 'test') {
+        gelbooru.getRandomImage(SFW_TAG, (err, result) => {
+            msg.channel.send(result.file_url);
+        });
     }
 })
 
---
+++ gelbooru.js
@@ -0,0 +1,59 @@
+const https = require('https');
+
+const apiRoot = 'https:\/\/gelbooru.com/index.php?page=dapi&s=post&q=index';
+const Gelbooru = {
+
+    getTopImage: (tags, callback) => {
+        const url = `${apiRoot}&tags=${tags}&limit=1&json=1`;
+        getData(url, callback);
+    },
+
+    getRandomImage: (tags, callback) => {
+        getImageCount(tags, (count) => {
+            const pageNumber = parseInt(((Math.random() * count) + 1), 10);
+            const url = `${apiRoot}&tags=${tags}&limit=1&json=1&pid=${pageNumber}`;
+            
+            getData(url, (response) => {
+                if (!response || response.length < 1) {
+                    callback('no images found');
+                }
+
+                // Turn the file_url into a http link
+                if (response[0].file_url) {
+                    response[0].file_url = 'https:' + response[0].file_url;
+                }
+                callback(null, response[0]);
+            })
+        })
+    }
+}
+
+function getImageCount(tags, callback) {
+    var url = `${apiRoot}&tags=${tags}&limit=0`;
+
+    getData(url, (response) => {
+        callback(/(?:count=")([0-9]+)/.exec(response)[1]);
+    })
+}
+
+function getData(url, callback) {
+    const isJson = url.indexOf('&json=1') > -1;
+    https.get(url, (res) => {
+        res.setEncoding('utf8');
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            if (isJson) {
+                callback(JSON.parse(data));
+            } else {
+                callback(data);
+            }
+        })
+    });
+}
+
+module.exports = Gelbooru;
